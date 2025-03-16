77495e0bdfbda7ab179a52aec7c0c72c4e554e9a
chingwu99 <henry88028802@hotmail.com>
2025-03-16 22:38:16 +0800

    feat: update domain

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['http://localhost:8080', 'http://localhost:3000'],
+    origin: ['http://localhost:8080', 'http://localhost:3000', 'https://interact.up.railway.app/'],
     credentials: true,
   })
 )
